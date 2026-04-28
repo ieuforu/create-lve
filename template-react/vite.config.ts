35337a4907dec2cabd6042d9eb99810428834c80
ieuforu <ieuforu@gmail.com>
2026-04-28 16:03:09 +0800

    0.3.3

--- template-react/vite.config.ts
+++ template-react/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: env.VITE_DIFY_API_URL,
+        target: env.VITE_BASE_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
