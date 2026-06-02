df1501ec64e1ffbdd6e8a5f2112817306e14adf8
ieuforu <ieuforu@gmail.com>
2026-06-02 15:33:39 +0800

    fix: remove duplicate style.css import from templates
    CSS_STRATEGIES in config.js already injects the CSS import during transform.
    Having it in both the template and the transform caused duplicates.

--- template-react/src/main.tsx
+++ template-react/src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client'
 import { Providers } from './app/providers'
 import { router } from './app/router'
 import { RouterProvider } from 'react-router'
-import './style.css'
 
 createRoot(document.getElementById('root')!).render(
   <Providers>
