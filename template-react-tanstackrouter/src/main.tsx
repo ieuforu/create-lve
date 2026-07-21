eb64cc1bebb58e887e35030e0ff78e00bab71eba
ieuforu <ieuforu@gmail.com>
2026-07-21 17:27:47 +0800

    fix(tanstack): add Jotai Provider and fix React 19 compatibility
    - Wrap app with Jotai Provider in main.tsx for global store support
    - Add undefined initial value to useRef in UsersPage (React 19 types)
    - Remove unused Mail import from UserDetailPage

--- template-react-tanstackrouter/src/main.tsx
+++ template-react-tanstackrouter/src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { StrictMode } from 'react'
+import { Provider as JotaiProvider } from 'jotai'
 import { RouterProvider } from '#/lib/providers/RouterProvider.tsx'
 import { QueryProvider } from '#/lib/providers/QueryProvider.tsx'
 
@@ -10,9 +11,11 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <QueryProvider>
-        <RouterProvider />
-      </QueryProvider>
+      <JotaiProvider>
+        <QueryProvider>
+          <RouterProvider />
+        </QueryProvider>
+      </JotaiProvider>
     </StrictMode>,
   )
 }
