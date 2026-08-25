387ef26a826c633681652f54059c7cd940a87fa6
ieuforu <ieuforu@gmail.com>
2026-08-25 17:06:08 +0800

    chore: add rolldown chunk splitting to vue template

--- template-vue/vite.config.ts
+++ template-vue/vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          minSize: 10000,
+          groups: [
+            {
+              name: 'vendor-vue',
+              test: /node_modules[/]vue(?!-router)/,
+              priority: 35,
+            },
+            {
+              name: 'vendor-router',
+              test: /node_modules[/]vue-router/,
+              priority: 30,
+            },
+            {
+              name: 'vendor-ui',
+              test: /node_modules[/]reka-ui/,
+              priority: 25,
+            },
+            {
+              name: 'vendor-state',
+              test: /node_modules[/]pinia/,
+              priority: 20,
+            },
+            {
+              name: 'vendor',
+              test: /node_modules/,
+              priority: 10,
+            },
+          ],
+        },
+      },
+    },
+  },
 })
